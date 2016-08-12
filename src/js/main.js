3888fd3f160580a4d3c298f0bfc52f0e2d641c00
jdalton308 <jdalton308@yahoo.com>
2016-08-12 13:08:54 -0600

    Progress commit. Work on sorter selectors

--- src/js/main.js
+++ src/js/main.js
@@ -234,17 +234,166 @@ $(function(){
   // ------------------------
   var scaleeSorter = (function(){
 
+    // Elements
+    //------------
     var $slide1 = $('.slide1');
     var $openFilter = $('#open-filter');
+    var $wheelSelectors = $('.wheel-selector');
+    var $filterItems = $wheelSelectors.children('.item');
+    var $sortTrigger = $('#sort-me');
 
+    var wheelHeight = $wheelSelectors.height();
+
+    console.log('Wheel height: '+ wheelHeight);
+
+    var $items = $('.item');
+    var itemHeight = $items.innerHeight();
+
+    console.log('Item height: '+ itemHeight);
+
+    var $selectorTitle = $('.selector-title');
+    var titleHeight = $selectorTitle.innerHeight();
+
+    console.log('Title height: '+ titleHeight);
+
+    // State Variables
+    //-----------------
+    var filters = [];
+    var panelOpen = $slide1.hasClass('edit-mode');
+
+
+    // Bind events
+    //------------
     function eventsOn() {
+
+      // Open/close filter panel
       $openFilter.click(function(){
-        $slide1.toggleClass('edit-mode');
+        console.log('click');
+        if (panelOpen) {
+          console.log('closing panel...');
+          closePanel();
+        } else {
+          console.log('opening panel...');
+          openPanel();
+        }
+
+        panelOpen = !panelOpen;
+      });
+
+      // Select filters
+      $filterItems.click(function(){
+        $(this).addClass('selected').siblings().removeClass('selected');
+      });
+
+      // Sort trigger
+      $sortTrigger.click(function(){
+        // close panel
+        closePanel();
+
+        // Trigger sort
       });
     }
 
+    // function wheelSelector() {
+
+    //   function centerList($wheelSelector) {
+    //     var itemCount = $wheelSelector.children('.item').length;
+    //     var listHeight = itemHeight * listHeight;
+    //     var hiddenArea = listHeight - wheelHeight;
+
+    //     console.log('List height: '+ listHeight);
+
+    //     if (hiddenArea > 0) {
+    //       var topPos = hiddenArea/2;
+    //       $wheelSelector.style('top', topPos);
+    //     }
+    //   }
+
+    //   function initWheels() {
+    //     $wheelSelectors.each(function(){
+    //       var $this = $(this);
+    //       centerList($this);
+    //     });
+    //   }
+
+    //   initWheels();
+    // }
+
+    // Open Bar
+    //------------
+    function openPanel() {
+      $slide1.addClass('edit-mode');
+
+      // Change height of 'wheel-selector'
+      function openFilters() {
+        var openHeight = itemHeight * 4; // arbitrary
+        $wheelSelectors.css('height', openHeight+'px');
+      }
+
+      openFilters();
+    }
+
+    // Collapse bar
+    //---------------
+    function closePanel() {
+      $slide1.removeClass('edit-mode');
+
+      // Get and position selected items
+      function getAndSlideSelection() {
+
+        // Get selected elements
+        var $selectedFilters = $('.selectors .selected');
+
+        // reset filters
+        filters = [];
+
+        // For each selection, slide up, and save value
+        $selectedFilters.each(function(){
+          var $this = $(this);
+          slideUpSelection($this);
+
+          // Store value
+          var filterValue = $this.text();
+          filters.push(filterValue);
+        });
+
+      }
+
+
+      // Move selected items to top
+      function slideUpSelection($item) {
+        // Get scrollTop() of selection
+        var scrollPos = $item.position().top;
+
+        console.log('selected position: ');
+        console.log($item.position());
+
+        console.log('Scrolling to '+ scrollPos);
+
+        // Scroll wheel-selector to that point
+        // $item.parents('.wheel-selector').animate({
+        //   scrollTop: scrollPos
+        // }, 300);
+
+        $item.parents('.wheel-selector').scrollTop(scrollPos);
+      }
+
+
+      // Change height of 'wheel-selector' to only show one item
+      function collapseFilters() {
+        $wheelSelectors.css('height', itemHeight+'px');
+      }
+
+      getAndSlideSelection();
+      collapseFilters();
+    }
+
+
+    // Initialize sorter
+    //------------------
     function init() {
       eventsOn();
+      closePanel();
     }
 
     return {
