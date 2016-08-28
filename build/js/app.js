b842bf87f5e1dc69672408821153bad5a5f5471b
jdalton308 <jdalton308@yahoo.com>
2016-08-27 19:29:08 -0600

    Progress commit: More work on scroll animations

--- build/js/app.js
+++ build/js/app.js
@@ -12269,6 +12269,7 @@ function scrollAnimate() {
 	var intro_dur = 1;
 	var intro_delay = 1;
 
+
 	// Landing text animation
 	//-----------------------
 	var title1_tl = new TimelineMax();
@@ -12277,19 +12278,8 @@ function scrollAnimate() {
 	title1_tl.to(title1_h1, intro_dur, {opacity: 1, top: 0}, intro_delay);
 	title1_tl.to(title1_h2, intro_dur, {opacity: 1, top: 0}, intro_delay);
 	title1_tl.to(title1_line, 0.8, {opacity: 1, ease: Expo.easeOut});
-	title1_tl.to(slide1, 2, {height: '80vh', ease: Elastic.easeOut.config(1, 0.5)});
+	// title1_tl.to(slide1, 2, {height: '80vh', ease: Elastic.easeOut.config(1, 0.5)});
 
-	// Landing floating polygon
-	//--------------------------
-	var title1_poly = TweenMax.to(landingPoly, 1, {right:'90%', top:'50%', scale: 0.7, rotation:'80deg'});
-
-	var landingPolyScene = new ScrollMagic.Scene({
-			triggerElement: slide1,
-			triggerHook: 'onLeave',
-			duration: '100%'
-		})
-		.setTween(title1_poly)
-		.addTo(controller);
 
 	// Landing scroll-out
 	//-------------------------
@@ -12297,10 +12287,10 @@ function scrollAnimate() {
 
 	var title1_tlout = new TimelineMax();
 	// title1_tlout.to(title1_el, title_out_dur, {top: '150px'}, 0);
-	title1_tlout.to(title1_num, title_out_dur, {top: '-150px'}, 0);
-	title1_tlout.to(title1_h1, title_out_dur, {top: '-70px'}, 0);
-	title1_tlout.to(title1_line, title_out_dur, {top: '0px'}, 0);
-	title1_tlout.to(title1_h2, title_out_dur, {top: '80px'}, 0);
+	title1_tlout.to(title1_num, title_out_dur, {top: '-150px', opacity:0}, 0);
+	title1_tlout.to(title1_h1, title_out_dur, {top: '-70px', opacity:0}, 0);
+	title1_tlout.to(title1_line, title_out_dur, {top: '0px', opacity:0}, 0);
+	title1_tlout.to(title1_h2, title_out_dur, {top: '80px', opacity:0}, 0);
 
 	var landingTextScene = new ScrollMagic.Scene({
 			triggerElement: slide1,
@@ -12324,20 +12314,43 @@ function scrollAnimate() {
 	var scalee_bgel = $scaleeBg.get(0);
 	var scaleeCont_el = $slide2.find('.scalee-cont').get(0);
 
+	// Toggle active class
+	//----------------------
+	var scaleeClassScene = new ScrollMagic.Scene({
+			triggerElement: slide2_el,
+			triggerHook: '0.6'
+		})
+		.setClassToggle(slide2_el, 'active')
+		.addTo(controller);
+
 	// Adjust background height
 	//--------------------------
-	var sorter_tl = new TimelineMax();
-	sorter_tl.to(scalee_bgel, 1, {height:'40%'}, 0);
-	sorter_tl.to(scaleeCont_el, 1, {top:'15vh'}, 0);
+	var sorter_tween = TweenMax.to(scalee_bgel, 1, {height:'40%'});
 
 	var scaleeScene = new ScrollMagic.Scene({
 			triggerElement: slide2_el,
-			triggerHook: '0.8',
-			duration: '60%'
+			triggerHook: '0.5',
+			duration: '100%'
 		})
-		.setTween(sorter_tl)
+		.setTween(sorter_tween)
 		.addTo(controller);
 
+
+	//-----------------------
+	// Small airplane-like polygon in forefront
+	//--------------------------
+	var smallPoly_tl = new TimelineMax();
+		smallPoly_tl.to(landingPoly, 1, {right:'100%', top:'-20%', scale:0.7, rotation:'80deg', zIndex:0});
+		smallPoly_tl.to(landingPoly, 3, {right:'60%', top:'-5%', scale:0.5, rotation:'130deg'});
+	// var title1_poly = TweenMax.to(landingPoly, 1, {right:'90%', top:'50%', scale: 0.7, rotation:'80deg'});
+
+	var landingPolyScene = new ScrollMagic.Scene({
+			triggerElement: slide1,
+			triggerHook: 'onLeave',
+			duration: '400%'
+		})
+		.setTween(smallPoly_tl)
+		.addTo(controller);
 
 
 	//------------------
@@ -12345,15 +12358,15 @@ function scrollAnimate() {
 	//-----------------------
 	// - Fix to window and rotate during scroll
 
-	var largePoly_el = document.getElementById('large-poly');
-
+	var largePoly_el = $('#large-poly')[0];
 	var poly_tl = new TimelineMax();
-	poly_tl.to(largePoly_el, 1, {rotation:'90deg', top:'-20%'});
+		poly_tl.to(largePoly_el, 1, {rotation:'15deg', top:'-5%', left:'-50%'});
+		poly_tl.to(largePoly_el, 3, {rotation:'30deg', top:'0%', left:'-40%', scale:1.2});
 
 	var polyScene = new ScrollMagic.Scene({
-			triggerElement: slide1, // start immediately
-			triggerHook: 'onLeave',
-			duration: 0 // last entire page
+			triggerElement: slide2_el,
+			triggerHook: '1',
+			duration: '400%' // last entire page
 		})
 		.setTween(poly_tl)
 		.addTo(controller);
@@ -12405,10 +12418,104 @@ function scrollAnimate() {
 		return newTitleScene;
 	}
 
-	// Create scenes
-	var titleScene2 = newTitleScene($('#title2'));
-	var titleScene3 = newTitleScene($('#title3'));
+	// Create .title-box scenes
+	// var titleScene2 = newTitleScene($('#title2'));
+	// var titleScene3 = newTitleScene($('#title3'));
 
+
+
+	//------------------
+	// Say Hello CTAs
+	//-----------------------
+
+	// Constructor function
+	function newHelloBoxScene($boxEl) {
+		// Move icon
+		// Leave text
+		// Move whole el
+		// Move polygon behind
+		var box_el = $boxEl.get(0);
+		var icon_el = $boxEl.find('.icon').get(0);
+		var h5_el = $boxEl.find('h5').get(0);
+		var p_el = $boxEl.find('p').get(0);
+
+		var step1_dur = 1;
+		var step2_dur = 1;
+
+		var hello_tl = new TimelineMax();
+		// - inward animations
+		hello_tl.to(icon_el, step1_dur, {top: 0}, 0);
+		hello_tl.to(h5_el, step1_dur, {textShadow:'0 0 0 transparent'}, 0);
+		hello_tl.to(p_el, step1_dur, {textShadow:'0 0 0 transparent'}, 0);
+		// - outward animations
+		hello_tl.to(icon_el, step2_dur, {top:'-50px'}, step1_dur);
+		hello_tl.to(h5_el, step2_dur, {textShadow:'0 0 10px #FFF'}, step1_dur);
+		hello_tl.to(p_el, step2_dur, {textShadow:'0 0 10px #FFF'}, step1_dur);
+
+		var boxScene = new ScrollMagic.Scene({
+				triggerElement: box_el,
+				triggerHook: 'onEnter',
+				duration: '100%'
+			})
+			.setTween(hello_tl)
+			.addTo(controller);
+
+		return boxScene;
+	}
+
+	// Create box scenes
+	// $('.slide2 .nav-box').each(function(){
+	// 	newHelloBoxScene( $(this) );
+	// });
+
+
+
+	//------------------
+	// Job Postings
+	//-----------------------
+	// - blur in and out text: title and p
+	// - move button in and out of position
+	// - 
+
+	// Constructor
+	function newJobPostingScene($postEl) {
+
+		// - elements
+		var post_el = $postEl.get(0);
+		var $titleBox = $postEl.find('.job-title-box');
+		var $content = $postEl.find('posting-description');
+		var content_el = $content.get(0);
+		var $buttonRow = $postEl.find('.button-row');
+		var button_el = $buttonRow.find('.button').get(0);
+
+		var in_duration = 1;
+		var pause_duration = 5;
+		var out_duration = 1;
+		var outDelay = pause_duration + out_duration;
+
+		// - create timeline
+		var job_tl = new TimelineMax();
+		job_tl.to(content_el, in_duration, {textShadow: '0 0 0 transparent'}, 0);
+		job_tl.to(button_el, in_duration, {top: 0}, 0);
+		job_tl.to(content_el, out_duration, {textShadow: '0 0 15px #000'}, outDelay);
+		job_tl.to(button_el, out_duration, {top:'-100px'}, outDelay);
+
+		// Create Scene
+		var jobScene = new ScrollMagic.Scene({
+				triggerElement: post_el,
+				triggerHook: 'onEnter',
+				duration: '100%'
+			})
+			.setTween(job_tl)
+			.addTo(controller);
+
+		return jobScene;
+	}
+
+	// Create scene for each job posting
+	// $('.slide3 .job-posting').each(function(){
+	// 	newJobPostingScene( $(this) );
+	// });
 }
 
 
